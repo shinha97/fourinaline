686e2301e290f45b44a0090aa609f653fd790d4e
shinha97 <shinha97@gmail.com>
2018-03-05 00:34:39 -0800

    Created new node to update state, fixed isValidMove indexing

--- Board.java
+++ Board.java
@@ -36,14 +36,14 @@ public class Board{
         // 					          {'_','_','_','_','_','_','_','_'}};
     }
     public Board(char[][] b){
-      board = new char[8][8];
+        board = new char[8][8];
 
-      //populate empty board
-      for(int i=0;i<board.length;i++){
-          for(int j=0;j<board[i].length;j++){
-              board[i][j] = b[i][j];
-          }
-      }
+        //populate empty board
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[i].length;j++){
+                board[i][j] = b[i][j];
+            }
+        }
     }
     public int isTerminal(){
         //check for rows for X or O victory, check 5 positions across 8 columns
@@ -83,63 +83,66 @@ public class Board{
         return 0;
     }
     public char[][] getBoard(){
-      return board;
+        return board;
     }
     public int evaluate(char c){
-        char enemy;
-        if(c=='X') enemy = 'O';
-        else enemy = 'X';
         //TODO: Daniel please write a kick-ass eval function here
         //Given a board, call it's eval function to give a rating on its desirability
+        int score = 0;
+        for(int col=0;col<board.length-2;col++){
+            for(int row=0;row<board.length;row++){
+                if(board[row][col]==c){
+                    if(board[row][col]+1<board.length);
+                }
+            }
+        }
 
-        int fours = checkFours(c);
-        if(fours != 0) return 1000;
-        int enemyFours = checkFours(enemy);
-        if(enemyFours != 0) return -1000;
-        int threes = checkThrees(c);
-        int twos = checkTwos(c);
-        int enemyThrees = checkThrees(enemy);
-        int enemyTwos = checkTwos(enemy);
-
-        return threes + twos - enemyThrees - enemyTwos;
-    }
-    private int checkFours(char c){
-      //check for rows for X or O victory, check 5 positions across 8 columns
-      for(int col=0;col<board.length-winCount+1;col++){
-          for(int row=0;row<board.length;row++){
-              if(board[row][col]==board[row][col+1] &&
-                      board[row][col+1]==board[row][col+2] &&
-                      board[row][col+2]==board[row][col+3]){
-                  if(board[row][col]==c){
-                      // System.out.println("ROW: row "+(char)('@'+row+1)+" col "+(col+1));
-                      return 1;
-                  }
-              }
-          }
-      }
-      //check columns for X or O victory, check 5 positions across 8 rows
-      for(int row=0;row<board.length-winCount+1;row++){
-          for(int col=0;col<board.length;col++){
-              if(board[row][col]==board[row+1][col] &&
-                      board[row+1][col]==board[row+2][col] &&
-                      board[row+2][col]==board[row+3][col]){
-                  if(board[row][col]=='X'){
-                      // System.out.println("COLUMN: row "+(char)('@'+row+1)+" col "+(col+1));
-                      return 1;
-                  }
-                  if(board[row][col]=='O'){
-                      // System.out.println("COLUMN: row "+(char)('@'+row+1)+" col "+(col+1));
-                      return -1;
-                  }
-              }
-          }
-      }
-      return -1;
+        //check for rows for X or O victory, check 5 positions across 8 columns
+        for(int col=0;col<board.length-winCount+1;col++){
+            for(int row=0;row<board.length;row++){
+                if(board[row][col]==board[row][col+1] &&
+                        board[row][col+1]==board[row][col+2] &&
+                        board[row][col+2]==board[row][col+3]){
+                    if(board[row][col]=='X'){
+                        // System.out.println("ROW: row "+(char)('@'+row+1)+" col "+(col+1));
+                        return 1;
+                    }
+                    if(board[row][col]=='O'){
+                        // System.out.println("ROW: row "+(char)('@'+row+1)+" col "+(col+1));
+                        return -1;
+                    }
+                }
+            }
+        }
+        //check columns for X or O victory, check 5 positions across 8 rows
+        for(int row=0;row<board.length-winCount+1;row++){
+            for(int col=0;col<board.length;col++){
+                if(board[row][col]==board[row+1][col] &&
+                        board[row+1][col]==board[row+2][col] &&
+                        board[row+2][col]==board[row+3][col]){
+                    if(board[row][col]=='X'){
+                        // System.out.println("COLUMN: row "+(char)('@'+row+1)+" col "+(col+1));
+                        return 1;
+                    }
+                    if(board[row][col]=='O'){
+                        // System.out.println("COLUMN: row "+(char)('@'+row+1)+" col "+(col+1));
+                        return -1;
+                    }
+                }
+            }
+        }
+        return -1;
     }
 
     public static int getCell(int x, int y) {
         return board[x][y];
     }
+
+    public char[][] updateBoard(Point newPosition, char value){
+        board[newPosition.x][newPosition.y] = value;
+        return board;
+    }
+
     public void printBoard(int startPlayer){
         for(int col=0;col<9;col++){//column
             for(int row=0;row<9;row++){//row
@@ -157,4 +160,6 @@ public class Board{
             }System.out.println();
         }
     }
+
+
 }
--- Main.java
+++ Main.java
@@ -1,50 +1,50 @@
 import java.util.Scanner;
 
 public class Main{
-  public static void main(String[] args){
-      //=========Set variables Area==========
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter the amount of time (in sec) allowed for generating a move: ");
+    public static void main(String[] args){
+        //=========Set variables Area==========
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the amount of time (in sec) allowed for generating a move: ");
 
-      int time = input.nextInt();
-      System.out.print("Choose starting player (0 for Player, 1 for Opponent): ");
-      int startPlayer = input.nextInt();
-      //========End set variables Area==========
+        int time = input.nextInt();
+        System.out.print("Choose starting player (0 for Player, 1 for Opponent): ");
+        int startPlayer = input.nextInt();
+        //========End set variables Area==========
 
 
-      Board b = new Board();
-      b.isTerminal();
-      b.printBoard(startPlayer);
+        Board b = new Board();
+        b.isTerminal();
+        b.printBoard(startPlayer);
 
-      if (startPlayer == 0) System.out.print("\nPlayer's move is: ");
-      else System.out.print("\nOpponent's move is: ");
+        if (startPlayer == 0) System.out.print("\nPlayer's move is: ");
+        else System.out.print("\nOpponent's move is: ");
 
-      String move = input.next().toLowerCase();
-      if (isValidMove(b,move)){
-          System.out.println("Valid move!");
-      }
-      else System.out.println("Invalid move..");
-  }
+        String move = input.next().toLowerCase();
+        if (isValidMove(b,move)){
+            System.out.println("Valid move!");
+        }
+        else System.out.println("Invalid move..");
+    }
 
-  /**======================
-   *    HELPER FUNCTIONS
-   * ======================*/
+    /**======================
+     *    HELPER FUNCTIONS
+     * ======================*/
 
-  public static boolean isValidMove(Board b,String move){
-      if (((int)move.charAt(0) > 96) && ((int)move.charAt(0) < 105) && (move.charAt(1) >= 0) && (move.charAt(1) - '0' < 9)){//if the row/col is valid (input: a-h,1-8)
-              if (b.getCell(((int)move.charAt(0)) -97,move.charAt(1) - '0') == '_'){//the move is an empty ti5le
-                  //DO STUFF
-                  return true;
-              }
-              else{
-                  System.out.println("This is not an empty space. Try again.");
-                  return false;
-              }
-      }
-      else{
-          System.out.println("Error: Move out of bounds. Try again. " + (int)move.charAt(0) + " " + move.charAt(1));
-          return false;
-      }
+    public static boolean isValidMove(Board b,String move){
+        if (((int)move.charAt(0) > 96) && ((int)move.charAt(0) < 105) && (move.charAt(1) >= 0) && (move.charAt(1) - '0' < 9)){//if the row/col is valid (input: a-h,1-8)
+            if (b.getCell(((int)move.charAt(0)) -97,move.charAt(1) - '0' -1) == '_'){//the move is an empty ti5le
+                //DO STUFF
+                return true;
+            }
+            else{
+                System.out.println("This is not an empty space. Try again.");
+                return false;
+            }
+        }
+        else{
+            System.out.println("Error: Move out of bounds. Try again. " + (int)move.charAt(0) + " " + move.charAt(1));
+            return false;
+        }
 
-  }
+    }
 }
