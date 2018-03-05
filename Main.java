127eea1751ee99aaba9fdf87aab29bdacc744ed5
Daniel <ledaniel89@yahoo.com>
2018-03-04 22:25:35 -0800

    working isValidMove help function

--- Main.java
+++ Main.java
@@ -20,9 +20,31 @@ public class Main{
       else System.out.print("\nOpponent's move is: ");
 
       String move = input.next().toLowerCase();
-      if (Board.isValidMove(move)){
+      if (isValidMove(b,move)){
           System.out.println("Valid move!");
       }
       else System.out.println("Invalid move..");
+  }
+
+  /**======================
+   *    HELPER FUNCTIONS
+   * ======================*/
+
+  public static boolean isValidMove(Board b,String move){
+      if (((int)move.charAt(0) > 96) && ((int)move.charAt(0) < 105) && (move.charAt(1) >= 0) && (move.charAt(1) - '0' < 9)){//if the row/col is valid (input: a-h,1-8)
+              if (b.getCell(((int)move.charAt(0)) -97,move.charAt(1) - '0') == '_'){//the move is an empty ti5le
+                  //DO STUFF
+                  return true;
+              }
+              else{
+                  System.out.println("This is not an empty space. Try again.");
+                  return false;
+              }
+      }
+      else{
+          System.out.println("Error: Move out of bounds. Try again. " + (int)move.charAt(0) + " " + move.charAt(1));
+          return false;
+      }
+
   }
 }
